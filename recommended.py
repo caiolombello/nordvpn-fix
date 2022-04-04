de265eebe8f8c1252514e4d23b2cf7c6d06dc0f0
Caio Barbieri <cbarbieri@vertigo.com.br>
2022-04-04 13:51:25 -0300

    First commit

---
+++ recommended.py
@@ -0,0 +1,12 @@
+from time import sleep
+from helium import * 
+from bs4 import BeautifulSoup
+
+url = 'https://nordvpn.com/servers/tools/'
+browser = start_chrome(url, headless=True)
+sleep(0.5)
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+
+server = (soup.find('h5', {'class': 'Title mb-3'}).text)
+
+print(server)
