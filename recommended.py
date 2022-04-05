82f1f36e1ca7e7d36845724a4d2c6d82ea2f74cb
Caio Barbieri <caiolvbarbieri@pm.me>
2022-04-05 16:39:17 -0300

    fix: +0.5 sleep time

--- recommended.py
+++ recommended.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://nordvpn.com/servers/tools/'
 browser = start_chrome(url, headless=True)
-sleep(0.5)
+sleep(1)
 soup = BeautifulSoup(browser.page_source, 'html.parser')
 
 server = (soup.find('h5', {'class': 'Title mb-3'}).text)
